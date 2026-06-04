ac146bcac85f2f2df1f448e3a5edff0fe74fa30a
Jerintresvance <jerinkmathew@tresvance.com>
2026-06-04 22:04:08 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://church-expense-tracker.onrender.com/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
